9bddceb5ac519a22c023aa65d4f0e8c29c2db193
Florian Hanke <admin@floeres-macbook.local>
2008-08-10 09:19:26 +0200

    + view spec

--- spec/helpers/view_spec.rb
+++ spec/helpers/view_spec.rb
@@ -3,10 +3,27 @@ require File.join(File.dirname(__FILE__), '../spec_helper')
 require 'helpers/view'
 
 describe Representers::Helpers::View do
-
-  # couldn't figure out how to test that
-  # if Representers::Base.helper Representers::Helpers::View 
-  # is called -> test that all_view_helpers.each are included in Base-Class
-  # somehow I don't understand flexmock
+  
+  class TestClass; end
+  
+  describe "including it" do
+    it "should include all the view helpers" do
+      in_the TestClass do
+        include Representers::Helpers::View
+      end
+      
+      TestClass.should include(ActionView::Helpers::ActiveRecordHelper)
+      TestClass.should include(ActionView::Helpers::TagHelper)
+      TestClass.should include(ActionView::Helpers::FormTagHelper)
+      TestClass.should include(ActionView::Helpers::FormOptionsHelper)
+      TestClass.should include(ActionView::Helpers::FormHelper)
+      TestClass.should include(ActionView::Helpers::UrlHelper)
+      TestClass.should include(ActionView::Helpers::AssetTagHelper)
+      TestClass.should include(ActionView::Helpers::PrototypeHelper)
+      TestClass.should include(ActionView::Helpers::TextHelper)
+      TestClass.should include(ActionView::Helpers::SanitizeHelper)
+      TestClass.should include(ERB::Util)
+    end
+  end
 
 end
