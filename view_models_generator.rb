8b9b8bde30c2017243ebb1f2e3f2be8221296a70
Florian Hanke <florian.hanke@gmail.com>
2010-01-13 12:28:49 +0100

    + check for class naming collisions

--- view_models_generator.rb
+++ view_models_generator.rb
@@ -3,25 +3,40 @@ class ViewModelsGenerator < Rails::Generator::Base
     
     record do |m|
       
+      # Check for class naming collisions.
+      #
+      m.class_collisions "ViewModels::#{class_name}"
+      
       # ViewModels
+      #
       m.directory 'app/view_models'
       m.template "view_models/view_model.rb", "app/view_models/#{file_name}.rb"
       
       # Specs
+      #
       m.directory "spec/app/view_models"
       m.template "spec/view_model_spec.rb", "spec/app/view_models/#{file_name}_spec.rb"
       
       # Views
+      #
       m.directory "app/views/view_models"
       m.directory "app/views/view_models/#{file_name}"
       m.template "views/_empty.html.haml", "app/views/view_models/#{file_name}/_list_item.html.haml"
       
       # Copy collection views.
+      #
       m.directory "app/views/view_models/collection"
       m.file "views/view_models/collection", "app/views/view_models/collection"
       
       # Show README.
+      #
       m.readme "README"
+      
+      # # View template for each action.
+      # actions.each do |action|
+      #   path = File.join('app/views', class_path, file_name, "#{action}.html.erb")
+      #   m.template 'view.html.erb', path,
+      #     :assigns => { :action => action, :path => path }
       
     end
   end
