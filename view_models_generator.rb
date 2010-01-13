f24f4a58bf87cfaa5ba96eae27dcf0fd389ee2e3
Florian Hanke <florian.hanke@gmail.com>
2010-01-13 12:18:31 +0100

    + moved collection views.

--- view_models_generator.rb
+++ view_models_generator.rb
@@ -8,16 +8,19 @@ class BasicAuthGenerator < Rails::Generator::Base
       m.template "view_models/view_model.rb", "app/view_models/#{file_name}.rb"
       
       # Specs
-      m.file "test/unit/user_test.rb", "test/unit/user_test.rb"
+      m.directory "spec/app/view_models"
+      m.template "spec/view_model_spec.rb", "spec/app/view_models/#{file_name}_spec.rb"
       
       # Views
       m.directory "app/views/view_models"
       m.directory "app/views/view_models/#{file_name}"
-      m.file "views/_empty.html.haml", "app/views/view_models/#{file_name}/_list_item.html.haml"
+      m.template "views/_empty.html.haml", "app/views/view_models/#{file_name}/_list_item.html.haml"
       
       # Copy collection views.
+      m.directory "app/views/view_models/collection"
+      m.file "views/view_models/collection", "app/views/view_models/collection"
       
-      
+      # Show README.
       m.readme "README"
       
     end
