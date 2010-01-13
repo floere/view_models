313dcadc00fae3418962beadaf164d944694ea70
Florian Hanke <florian.hanke@gmail.com>
2010-01-13 12:24:52 +0100

    !

--- init.rb
+++ init.rb
@@ -5,7 +5,5 @@ require 'view_models/active_record'
 require 'helpers/view'
 require 'helpers/rails'
 
-require 'view_models_generator'
-
 ActionController::Base.send :helper, ViewModels::Helper::Rails
 ActionController::Base.send :include, ViewModels::Helper::Rails
