4eb8f22a1b5baf1fa4459a4de1c5e891aafe7864
Florian Hanke <florian.hanke@gmail.com>
2010-02-10 17:37:35 +0100

    + include master helper module in metaclass

--- lib/view_models/view.rb
+++ lib/view_models/view.rb
@@ -2,8 +2,8 @@ module ViewModels
   class View < ActionView::Base
     
     def initialize controller, master_helper_module
-      view = super controller.class.view_paths, {}, controller
-      view.extend master_helper_module
+      (class << self; self; end).send :include, master_helper_module
+      super controller.class.view_paths, {}, controller
     end
     
   end
