bbbd474d01bef530da58945395142510a6f39b49
Florian Hanke <florian.hanke@gmail.com>
2010-02-11 11:40:23 +0100

    + metaclass explicity named

--- lib/view_models/view.rb
+++ lib/view_models/view.rb
@@ -2,7 +2,7 @@ module ViewModels
   class View < ActionView::Base
     
     def initialize controller, master_helper_module
-      (class << self; self; end).send :include, master_helper_module # unless included? master_helper_module
+      metaclass.send :include, master_helper_module # unless included? master_helper_module
       super controller.class.view_paths, {}, controller
     end
     
