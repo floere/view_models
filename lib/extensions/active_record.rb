5fdd20291b7c22f3e2094ec67ac613d65d1909b4
Florian Hanke <florian.hanke@gmail.com>
2010-03-02 16:16:19 +0100

    + doc

--- lib/extensions/active_record.rb
+++ lib/extensions/active_record.rb
@@ -6,6 +6,12 @@ module ViewModels
   module Extensions
     module ActiveRecord
       
+      # id and param are simply delegated to the model.
+      #
+      # This makes it possible to use the view_model
+      # for e.g. url generation:
+      # * edit_user_path(view_model)
+      #
       delegate :id, :to_param, :to => :model
       
       # Delegate to the action controller record identifier.
