f61853e71b1b507093b0798d304a2df39d8ed1c4
Florian Hanke <florian.hanke@gmail.com>
2010-07-18 18:37:46 +0200

    + Padrino switch

--- gem/lib/view_models.rb
+++ gem/lib/view_models.rb
@@ -1,9 +1,4 @@
-# TODO Switch to the right framework here.
-#
-# How would I recognize Padrino?
-#
-if defined?(Padrino)
-  # Bundler.setup :padrino
+if defined? Padrino
   require File.join(File.dirname(__FILE__), '/padrino/init')
 else
   require File.join(File.dirname(__FILE__), '/rails2/init')
