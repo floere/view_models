d696e2022753dcb3895a0b665acf07f2dee6f7f5
Florian Hanke <florian.hanke@gmail.com>
2010-07-21 00:20:58 +0200

    + rails 3 preparation

---
+++ gem/lib/rails3/init.rb
@@ -0,0 +1,18 @@
+# require 'experimental/modules_in_render_hierarchy'
+
+this = File.dirname __FILE__
+
+require File.join(this, '/lib/view_models')
+require File.join(this, '/../shared/init')
+require File.join(this, '/lib/view_models/base')
+require File.join(this, '/lib/view_models/view')
+
+require File.join(this, '/lib/helpers/view')
+require File.join(this, '/lib/helpers/collection')
+
+# TODO How was the view models generator included?
+#
+# require File.join(this, '/generators/view_models/view_models_generator')
+
+ActionController::Base.send :include, ViewModels::Helpers::Mapping
+ActionView::Base.send       :include, ViewModels::Helpers::Mapping
