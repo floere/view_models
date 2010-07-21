01bba2e80959568dfca9bb7800b294e8b301eb84
Florian Hanke <florian.hanke@gmail.com>
2010-07-21 10:46:25 +0200

    + rails 3 discovery

--- gem/lib/view_models.rb
+++ gem/lib/view_models.rb
@@ -1,5 +1,13 @@
+def rails3?
+  Rails && Rails.version >= '3.0.0'
+end
+
 if defined? Padrino
   require File.join(File.dirname(__FILE__), '/padrino/init')
 else
-  require File.join(File.dirname(__FILE__), '/rails2/init')
+  if rails3?
+    require File.join(File.dirname(__FILE__), '/rails3/init')
+  else
+    require File.join(File.dirname(__FILE__), '/rails2/init')
+  end
 end
--- rails235/config/initializers/view_models_gem.rb
+++ rails235/config/initializers/view_models_gem.rb
@@ -1,3 +1,3 @@
 # For development purposes.
 #
-require File.join(File.dirname(__FILE__), '/../../../gem/lib/rails2/init.rb')
+require File.join(File.dirname(__FILE__), '/../../../gem/lib/init.rb')
