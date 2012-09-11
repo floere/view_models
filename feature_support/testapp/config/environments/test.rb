de0e0b4a457bdf8fd738f0c7ffa8a4ffe5549a67
beatrichartz <beat.richartz@gmail.com>
2012-09-11 11:26:53 +0200

    ! some environment issues travis is stumbling over, second edition (?)

--- feature_support/testapp/config/environments/test.rb
+++ feature_support/testapp/config/environments/test.rb
@@ -20,7 +20,7 @@ Testapp::Application.configure do
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
   # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+  # config.active_record.mass_assignment_sanitizer = :strict
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
   # config.action_view.cache_template_loading = false
