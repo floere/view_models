21d67459dc3988bc9f0bf2d18ed2a2c00ce7cb43
beatrichartz <beat.richartz@gmail.com>
2012-10-05 12:04:48 +0200

    . template path has to be underscored, not only downcased

--- lib/view_models/base.rb
+++ lib/view_models/base.rb
@@ -188,7 +188,7 @@ module ViewModels
         # Note: Remembers the result since it is dependent on the Class name only.
         #
         def view_model_path
-          @view_model_path ||= self.name.gsub(/^ViewModels::|(\w+)(::)?/) { $1.downcase.pluralize + ($2 ? '/' : '').to_s if $1 }
+          @view_model_path ||= self.name.gsub(/^ViewModels::|(\w+)(::)?/) { $1.underscore.pluralize + ($2 ? '/' : '').to_s if $1 }
         end
         
     end
